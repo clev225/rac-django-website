7bf3dad088d394a6ffd278c23687dd90b3d64688
veronnnnn <veronicasulit91@gmail.com>
2025-03-27 15:12:38 +0800

    services footer fixed

--- informative_page/views.py
+++ informative_page/views.py
@@ -19,22 +19,7 @@ def team(request):
 def blogs(request):
     return render(request, "blog-post.html")
 
-<<<<<<< HEAD
 #SERVICES 
 
 def payroll_management(request):
     return render(request, "payroll_management.html")
-=======
-def blog_post_view(request):
-    # Get all published blog posts
-    blogs = BlogPost.objects.filter(is_published=True).order_by('-created_at')
-    
-    # For debugging
-    print(f"Number of blogs found: {blogs.count()}")
-    
-    return render(request, 'blog-post.html', {'blogs': blogs})
-
-def blog_detail_view(request, blog_id):
-    blog = get_object_or_404(BlogPost, id=blog_id, is_published=True)
-    return render(request, 'blog-read-more.html', {'blog': blog})
->>>>>>> d237d900ac2df7a7f36b9243237a1860e5353b76
