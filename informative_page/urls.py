d522578db5307652e0f7256f55db6a4550114b40
Cleven Javier <clevenjavier2021@gmail.com>
2025-04-30 09:48:07 +0800

    refactor(informative_page): rename FAQs to faqs for consistency
    Updated the URL, view function, and template references from 'FAQs' to 'faqs' to maintain consistent naming conventions across the codebase. This change improves code readability and maintainability.

--- informative_page/urls.py
+++ informative_page/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),  # Homepage
     path("about-us/", views.about_us, name="about_us"),
     path("services/", views.services, name="services"),
-    path("FAQs/", views.FAQs, name="FAQs"),
+    path("faq/", views.faqs, name="faqs"),
     #services get started button
     path('services/payroll-management/', views.payroll_management, name='payroll_management'),
     path('services/business-registration/', views.business_registration, name='business_registration'),
